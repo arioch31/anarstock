5a34297ca1d3aa412fe0925a7a587644324268ec
Adrian EGUZQUIZA <aeguzqui@e1r12p11.42.fr>
2015-12-10 11:32:55 +0100

    save libft

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aeguzqui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/28 18:40:23 by aeguzqui          #+#    #+#             */
+/*   Updated: 2015/12/09 15:00:59 by aeguzqui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *str, char (*f)(unsigned int, char))
+{
+	char			*dest;
+	unsigned int	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	dest = (char*)malloc((ft_strlen(str) + 1) * sizeof(char));
+	if (dest)
+	{
+		while (i < ft_strlen(str))
+		{
+			dest[i] = f(i, str[i]);
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
