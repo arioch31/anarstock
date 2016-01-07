7589869782f53f88f3fc6ad7f2fb6d6dbe1f0e6e
Adrian EGUZQUIZA <aeguzqui@e2r9p8.42.fr>
2016-01-07 19:51:01 +0100

    des trucs marchent

--- getnextline/main.c
+++ getnextline/main.c
@@ -11,14 +11,20 @@ int main(int ac, char **av)
 	{
 		fd= open(av[1], O_RDONLY);
 		while ((res =get_next_line(fd, &str)) &&res != -1)
+		{
+			ft_putnbr(res);
 			ft_putendl(str);
+		}
 		close(fd);
 		ft_putnbr(res);
 	}
 	if (ac ==1)
 	{
 		while ((res =get_next_line(0, &str)) &&res != -1)
+		{
+			ft_putnbr(res);
 			ft_putendl(str);
+		}
 		close(0);
 		ft_putnbr(res);
 	}
