ae659c956ff9ecd33cfb58a36b3e1bdcfaadcc9b
arioch <arioch@arioch-laptop.den>
2016-01-26 10:55:15 +0100

    envoi test

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -21,11 +21,11 @@ char    *arg_sub(const char *str)
     char    *charset;
     char    *charset2;
 
-    charset = "0123456789-+. $hlLjtz";
-    charset2 = "dDiuUoOxXeEfFaAgGcCsSp%";
-    sub = (char*)str;
-    if (*str != '%')
+    charset = "0123456789-+. $hlLjtz";//define?
+    charset2 = "dDiuUoOxXeEfFaAgGcCsSp%";//
+    if (!str || *str != '%')
         return (NULL);
+    sub = (char*)str;
     sub++;
     while (ft_strchr(charset, *sub))
         sub++;
@@ -45,20 +45,23 @@ t_list *arg_dissect(const char *str)
     ptr = (char*)str;
     while (*ptr)
     {
+        if(!ft_strchr(ptr, '%'))
+        {
+            if(*ptr)
+            ft_lstapp(&start, ft_lstnew(ptr, ft_strlen(ptr)));
+            return (start);
+        }
+        else //factoriser ca
+        {
         test = ft_strsub(ptr, 0, ft_strchr(ptr, '%') - ptr);
         ft_lstapp(&start, ft_lstnew(test, ft_strlen(test)));
         ptr = ft_strchr(ptr, '%');
         if (!(test = arg_sub(ptr)))
-            return(NULL);
+            return(NULL);//exit free?
         ft_lstapp(&start, ft_lstnew(test, ft_strlen(test)));
         ptr += ft_strlen(test);
-        if(!ft_strchr(ptr, '%'))
-        {
-            ft_lstapp(&start, ft_lstnew(ptr, ft_strlen(ptr)));
-            return (start);
         }
     }
-
     return (start);
 }
 
@@ -69,6 +72,7 @@ int	ft_printf(const char *str, ...)
     t_list  *l_vp;
     t_list  *start;
     t_list  *elem;
+    char    c;
     char	*tmp;
 
     if(!str)
@@ -86,34 +90,70 @@ int	ft_printf(const char *str, ...)
         return (0);
     // ft_lstaff(start);
     ft_lstaff(l_sp);
+    ft_putendl("test");
     start = l_sp;
     va_start(ap, str);
     l_vp = NULL;
     while(start)
     {
-        tmp = ((char*)start->content);
+       if ((tmp = ((char*)start->content)))
         if (*tmp== '%')
-        {
-            if (ft_strchr("dDioOuUxX", tmp[ft_strlen(tmp) - 1]))
-            elem= ft_lstnew(&va_arg(ap, int),sizeof(int));
-        //    else if (ft_strchr("aAeEfFgG", tmp[ft_strlen(tmp) - 1]))
-        //    elem= ft_lstnew(va_arg(ap, float),sizeof(float));
-            ft_lstapp(&l_vp, elem);
+        { 
+            ft_putendl("test");
+            c = *(tmp+1);
+            if (c == '%')
+            {
+                free(start->content);
+             ft_putendl("test3");
+             tmp = ft_strnew(2);
+                    *tmp = '%';
+            ft_putendl("test2");
+               start->content = tmp;
+            }
+            if (c == 'c')
+                {
+                    free(start->content);
+             ft_putendl("test3");
+             tmp = ft_strnew(2);
+                    *tmp = (char)va_arg(ap, int);
+            ft_putendl("test2");
+               start->content = tmp;
+                }      
+            else if (c == 'd' || c == 'i')
+                start->content = ft_itoa(va_arg(ap, int));
+       //       else if (c == 'u')
+         //       start->content = ft_itoa((unsigned int)va_arg(ap, int));
+           /* else if (c == 'o')
+                return (ft_printoctal(ap));
+            else if (c == 'x')
+                return (ft_printhexa(ap));
+            else if (c == 'p')
+                return (ft_printvoid(ap));
+            else if (c == '%')
+                return (ft_putchar(c));
+            else if (c == 's')
+                return (ft_printstr(ap));*/
         }
-        start = start->next;
-    }
-    va_end(ap);
-    ft_lstaff(l_vp);
-    tmp = ft_strnew(0);
-    while (start)
-    {
-        tmp = ft_strjoin(tmp, start->content);
-        start =start->next;
-    }
-    ft_putchar('\n');
-    ft_putendl(tmp);
+        //if (ft_strchr("dDioOuUxX", tmp[ft_strlen(tmp) - 1]))
+        //  elem= ft_lstnew(va_arg(ap, int),sizeof(int));
+        //else if (ft_strchr("aAeEfFgG", tmp[ft_strlen(tmp) - 1]))
+        //  elem= ft_lstnew(va_arg(ap, float),sizeof(float));
+           
+    start = start->next;
+}
+va_end(ap);
+ft_lstaff(l_sp);
+tmp = ft_strnew(0);
+start = l_sp;
+while (start)
+{
+    tmp = ft_strjoin(tmp, start->content);
+    start =start->next;
+}
+ft_putchar('\n');
+ft_putendl(tmp);
 
-    return (1);
+return (1);
 }
 
 int main (void)
@@ -126,6 +166,6 @@ int main (void)
     c = ';';
     str = "test ok";
 
-    d = ft_printf("%lutest de valist:%d %% %d", 8,9,256 , str);
+    d = ft_printf("%itest de%% valist:%d %i %c", 8,9,256 , 42);
     return (0);
 }
