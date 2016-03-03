321475c49b56d7c6fdee2d7dc9f701076188cdd0
ARIOCH <aeguzqui@student.42.fr>
2016-03-03 14:57:41 +0100

    v3 et brute en chantier

--- push_swap/brute.c
+++ push_swap/brute.c
@@ -12,29 +12,43 @@
 
 #include "push_swap.h"
 
-/*
+int				rewind(t_controleur *c)
+{
+
+}
+
 t_controleur	*opti_brute(t_controleur *parent, size_t it_max)
 {
 	int				i;
 	int				j;
+	t_controleur	fils;
 
 	if (!parent)
 		return (NULL);
-	if (parent->nb_op + parent->lb_len >= it_max)
-		return (NULL);
-	i = -1;
-	while (++i < 4)
+	while (1)
 	{
-		j = 1;
-		while (j < 4)
+		i = -1;
+		while (++i < 4)//changer ca
 		{
-			if ((TAB_F[i](j++, parent)))
-
-			else
+			j = 1;
+			while (j < 4)//ca aussi
+			{
+				TAB_F[i](j++, parent);
+				if (parent->nb_op + parent->lb_len >= it_max)
+					return (NULL); //rewind(parent)?
+				//condition de sortie ok
+				if (!parent->lb && pile_triee(parent->la, 1))
+				{
+					if (!(fils = opti_brute(copie_c(parent), parent->nb_op)))
+						return (parent);
+					else 
+						return (fils);
+				}
+			}
 		}
 	}
 }
-*/
+
 
 t_controleur	*brute(t_controleur *parent, size_t it_max)
 {
--- push_swap/solver.c
+++ push_swap/solver.c
@@ -37,6 +37,76 @@ int		solver(t_controleur *c)
 	return (solver(c));
 }
 
+void	mover(t_controleur *c)
+{
+	size_t test;
+	size_t test2;
+	size_t cpt;
+	size_t r1;
+	size_t r2;
+
+	cpt = 0;
+	r1 = 0;
+	r2 =0;
+	if (c->la_len > 1)
+		test = get_sublength(c->la->next, *(int*)c->la->content, 1);
+	if (c->lb_len > 1)
+		test2 = get_sublength(c->lb->next, *(int*)c->lb->content, 0);
+	if (!test2)
+	{
+		ps_swap(1, c);
+		while (++cpt < test)
+		{
+			ps_rot(1, c);
+			ps_swap(1, c);
+		}
+		while (--cpt > 1)
+			ps_revrot(1, c);
+	}
+	else if (!test)
+	{
+		ps_swap(2, c);
+		while (++cpt < test2)
+		{
+			ps_rot(2, c);
+			ps_swap(2, c);
+		}
+		while (--cpt > 1)
+			ps_revrot(2, c);
+	}
+	else
+	{
+		ps_swap(3, c);
+		while (++cpt < test && cpt < test2)
+		{
+			ps_rot(3, c);
+			ps_swap(3, c);
+			r1++;
+			r2++;
+		}
+		while (++cpt < test)
+		{
+			ps_rot(1, c);
+			ps_swap(1, c);
+			r1++;
+		}
+		while (++cpt < test2)
+		{
+			ps_rot(2, c);
+			ps_swap(2, c);
+			r2++;
+		}
+		while (--r1 > 1 && --r2 > 1)
+			ps_revrot(3, c);
+		while (--r1 > 1)
+			ps_revrot(1, c);
+		while (--r2 > 1)
+			ps_revrot(2, c);
+		
+
+	}	
+}
+
 int		solverv2(t_controleur *c)
 {
 	size_t test;
@@ -50,11 +120,12 @@ int		solverv2(t_controleur *c)
 			ps_push(2, c);
 		return (solverv2(c));
 	}
-	if (*(int*)c->la->content < c->val_m)
+	if (*(int*)c->la->content < c->val_m && !pile_triee(c->la))
 	{
 		ps_push(1, c);
 		return (solverv2(c));
 	}
+	
 	cpt = 0;
 	if (c->la_len > 1 \
 		&& (test = get_sublength(c->la->next, *(int*)c->la->content, 1)))
