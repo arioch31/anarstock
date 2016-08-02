410d7dfaab1ea3896075ad5ef2b5fd1b8d4ebed8
Adrian EGUZQUIZA <aeguzqui@e1r6p11.42.fr>
2016-08-02 05:39:19 +0200

    struct grid a modif

--- fdf/fdf.h
+++ fdf/fdf.h
@@ -6,12 +6,19 @@
 /*   By: aeguzqui <aeguzqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/28 20:48:35 by aeguzqui          #+#    #+#             */
-/*   Updated: 2016/07/08 00:19:10 by aeguzqui         ###   ########.fr       */
+/*   Updated: 2016/08/02 04:34:41 by aeguzqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _FDF_H
 # define _FDF_H
 # include "get_next_line.h"
+
+typedef struct	s_grid
+{
+	t_list		*grid;
+	int			rows;
+	int			lines;
+}				t_grid;
 
 #endif
--- fdf/main.c
+++ fdf/main.c
@@ -6,19 +6,20 @@
 /*   By: aeguzqui <aeguzqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/28 20:32:05 by aeguzqui          #+#    #+#             */
-/*   Updated: 2016/06/29 00:00:32 by aeguzqui         ###   ########.fr       */
+/*   Updated: 2016/08/02 04:35:47 by aeguzqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "get_next_line.h"
+#include "fdf.h"
 
-void	aff_grid(t_list *list)
+void	aff_grid(t_grid *grille)
 {
 	t_list	*ptr;
 	t_list	*ptr2;
 
-	ptr = list;
+	ptr = grille->grid;
 	while (ptr)
 	{
 		ptr2 = *(t_list**)ptr->content;
@@ -33,29 +34,39 @@ void	aff_grid(t_list *list)
 	}
 }
 
-int		main(int ac, char **av)
+t_grid	*new_grid(int fd)
 {
-	int		fd;
-	int		nb_rows;
 	char	*line;
 	t_list	**map;
-	t_list	*list;
+	t_grid	*grille;
 
+	grille = malloc(sizeof(t_grid));
 	line = NULL;
-	list = NULL;
-	nb_rows = 0;
-	if (ac != 2 || !(fd = open(av[1], O_RDONLY)))
-		return (0);
+	grille->grid = NULL;
+	grille->rows = 0;
+	grille->lines = 0;
 	while ((get_next_line(fd, &line)))
 	{
 		map = malloc(sizeof(t_list*));
-		if (!nb_rows)
-			nb_rows = nb_words(line, ' ');
-		if (nb_rows != nb_words(line, ' '))
-			return (0);
+		if (!grille->rows)
+			grille->rows = nb_words(line, ' ');
+		if (grille->rows != nb_words(line, ' '))
+			return (NULL);
 		*map = ft_lstrsplit(line, ' ');
-		ft_lstapp(&list, ft_lstnew(map, sizeof(t_list**)));
+		ft_lstapp(&(grille->grid), ft_lstnew(map, sizeof(t_list**)));
+		grille->lines++;
 	}
-	aff_grid(list);
+	return (grille);
+}
+
+int		main(int ac, char **av)
+{
+	int		fd;
+	t_grid	*grille;
+
+	if (ac != 2 || !(fd = open(av[1], O_RDONLY)))
+		return (0);
+	grille = new_grid(fd);
+	aff_grid(grille);
 	return (0);
 }
