985c695c22eec9f23bca819e0316230c186fdc77
Adrian EGUZQUIZA <aeguzqui@e1r4p13.42.fr>
2017-03-22 04:50:06 +0100

    print_line OK

--- ft_ls/start.c
+++ ft_ls/start.c
@@ -6,12 +6,15 @@
 /*   By: aeguzqui <aeguzqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/15 02:03:59 by aeguzqui          #+#    #+#             */
-/*   Updated: 2017/03/15 04:39:17 by aeguzqui         ###   ########.fr       */
+/*   Updated: 2017/03/22 04:49:14 by aeguzqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <strings.h>
+#include <dirent.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <sys/types.h>
@@ -29,6 +32,10 @@ void	write_rights(struct stat *ptr)
 		printf("%c", 'c');
 	else if (S_ISFIFO(ptr->st_mode))
 		printf("%c", 'p');
+	else if (S_ISSOCK(ptr->st_mode))
+		printf("%c", 's');
+	else if (S_ISLNK(ptr->st_mode))
+		printf("%c", 'l');
 	else
 		printf("%c", '-');
 	printf("%c%c%c%c%c%c%c%c%c",
@@ -41,28 +48,61 @@ void	write_rights(struct stat *ptr)
 	ptr->st_mode & S_IROTH ? 'r' : '-',
 	ptr->st_mode & S_IWOTH ? 'w' : '-',
 	ptr->st_mode & S_IXOTH ? 'x' : '-');
-	printf("%c ", ptr->st_mode & S_IFLNK ? '@' : ' ');
 }
 
-int	main(int ac, char **av)
+const char	*g_month[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+	"Aug", "Sep", "Oct", "Nov", "Dec"};
+
+void	write_date(struct tm *t)
+{
+	printf("%s %02d %02d:%02d", g_month[t->tm_mon],
+	t->tm_mday, t->tm_hour + 1, t->tm_min);
+}
+
+void	print_line(struct stat *ptr, char *name)
+{
+	char test[250];
+
+	write_rights(ptr);
+	printf(" %2hu ", ptr->st_nlink);
+	printf("%s  ", getpwuid(ptr->st_uid)->pw_name);
+	printf("%s", getgrgid(ptr->st_gid)->gr_name);
+	printf("%9lld ", ptr->st_size);
+	write_date(gmtime(&ptr->st_mtimespec.tv_sec));
+	printf(" %-s", name);
+	bzero(test, 250);
+	if (S_ISLNK(ptr->st_mode) && readlink(name, test, 250))
+		printf(" -> %s", test);
+	printf("\n");
+}
+
+void	print_dir_line(char *path)
+{
+	struct dirent	*dp;
+	DIR				*dirp;
+	struct stat		ptr;
+
+	dirp = opendir(path);
+	if (dirp)
+		while ((dp = readdir(dirp)) != NULL)
+		{
+			if (lstat(dp->d_name, &ptr) == 0)
+				print_line(&ptr, dp->d_name);
+			else
+				printf("%s failed\n", dp->d_name);
+		}
+	(void)closedir(dirp);
+}
+
+int		main(int ac, char **av)
 {
 	struct stat ptr;
 
 	if (ac > 1 && stat(av[1], &ptr) == 0)
 	{
-		printf("test\n");
-		printf("%s\n", ctime(&ptr.st_atimespec.tv_sec));
-		printf("%s\n", ctime(&ptr.st_mtimespec.tv_sec));
-		printf("%s\n", ctime(&ptr.st_ctimespec.tv_sec));
-
-		write_rights(&ptr);
-		printf("%hu ", ptr.st_nlink);
-		printf("%s  ", getpwuid(ptr.st_uid)->pw_name);
-		printf("%s", getgrgid(ptr.st_gid)->gr_name);
-		printf("%9lld\n", ptr.st_size);
-		printf("%s", ctime(&ptr.st_mtimespec.tv_sec));
+		print_dir_line(av[1]);
 	}
-	else
-		printf("%s failed\n", av[1]);
+	else if (ac == 1)
+		print_dir_line(".");
 	return (0);
 }
